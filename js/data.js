a4224bdaa557f496eae60462cce4aaee2d19d0f2
Victor Nizeyimana <me@mrclarify.com>
2018-12-26 20:56:44 +0000

    SReplace URL in HTML or text content with clickable link

--- js/data.js
+++ js/data.js
@@ -12,6 +12,18 @@ $.getJSON("../data/resources.json", function (jsonFromFile) {
          ],
     })
 });
+$(document).ready(function(){
+    function replace_content(content)
+    {
+    var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    var element_content=content.replace(exp_match, "<a href='$1'>$1</a>");
+    var new_exp_match =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var new_content=element_content.replace(new_exp_match, '$1<a target="_blank" href="http://$2">$2</a>');
+    return new_content;
+    }
+    var content = $('#table').html();
+    $('#table').html(replace_content(content));
+ });
 $(document).ready(function() {
     $("#searchInput").on("keyup", function() {
       const value = $(this).val().toLowerCase();
