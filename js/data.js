7176f617c3b9eb5beb356781f1d9941c5be07815
Victor Nizeyimana <me@mrclarify.com>
2018-12-26 21:51:51 +0000

    Small update

--- js/data.js
+++ js/data.js
@@ -1,33 +1,51 @@
-$.getJSON("../data/resources.json", function (jsonFromFile) {
-    const json = jsonFromFile;
-	$('#table').bootstrapTable({
-        data: json,
-        columns: [
-            { field: "title", title: "Title",  sortable: true, classes: true, uniqueId: true},
-            { field: "description", title: "Description", id: 'description'},
-            { field: "type", title: "Type"},
-            { field: "github", title: "On Github", classes:'github'},
-            { field: "npm", title: "On NPM", classes:'npm'},
-         ],
-    })
-});
 $(document).ready(function(){
-    function replace_content(content)
-    {
-    var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    var element_content=content.replace(exp_match, "<a href='$1'>$1</a>");
-    var new_exp_match =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    var new_content=element_content.replace(new_exp_match, '$1<a target="_blank" href="http://$2">$2</a>');
-    return new_content;
-    }
-    var content = $('#table').html();
-    $('#table').html(replace_content(content));
- });
-$(document).ready(function() {
-    $("#searchInput").on("keyup", function() {
-      const value = $(this).val().toLowerCase();
-      $("#table tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+    $.getJSON("../data/resources.json", function (jsonFromFile) {
+        const json = jsonFromFile;
+        $('#table').bootstrapTable({
+            data: json,
+            columns: [
+                { field: "title", title: "Title",  sortable: true, classes: true, uniqueId: true},
+                { field: "description", title: "Description", id: 'description'},
+                { field: "type", title: "Type"},
+                { field: "github", title: "Github", formatter},
+                { field: "npm", title: "NPM", formatter},
+                { field: "link", title: "URL", formatter},
+
+             ]
+        })
+        function github(value, row, index, field){
+            if(value == 1){
+                return '<i class="fab fa-github fa-stack-1x fa-inverse"></i>';
+            }else{
+                return '';
+            }
+        }
+        function formatter(value, row, index, field) {
+            if(field == 'link'){
+                return '<a href="'+ value +'">'+ value +'</a>';
+            }
+            if(field == 'github'){
+                if(value == 1){
+                    return '<i class="fab fa-github"></i>';
+                }else{
+                    return '<i class="fas fa-times"></i>';
+                }
+            }
+            if(field == 'npm'){
+                if(value == 1){
+                    return '<i class="fab fa-npm"></i>';
+                }else{
+                    return '<i class="fas fa-times"></i>';
+                }
+            }
+          }
     });
-  });
+
+
+    $("#searchInput").on("keyup", function() {
+        const value = $(this).val().toLowerCase();
+        $("#table tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+ });
